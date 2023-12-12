6cb30aa530e69a87aeeaa6d8ca2c737e2b5aaf36
Selviraja <i.am.selviraja.p@gmail.com>
2023-12-12 13:19:48 +0530

    Made some minor changes

--- src/contexts/TaskContext.js
+++ src/contexts/TaskContext.js
@@ -1,58 +1,6 @@
 import { createContext, useContext, useReducer } from 'react';
 import tasks from '../data';
-
-const TaskReducer = (tasks, action) => {
-  switch (action.type) {
-    case 'TOGGLE_DONE':
-      const toggledTasks = tasks.map((task) => {
-        if (task.id === action.id) {
-          return { ...task, isCompleted: !task.isCompleted };
-        }
-        return task;
-      });
-      return toggledTasks;
-
-    case 'EDIT_TASK':
-      const editedTasks = tasks.map((task) => {
-        if (task.id === action.id) {
-          return {
-            ...task,
-            title: action.editedTitle,
-            details: action.editedDetails,
-          };
-        }
-        return task;
-      });
-      return editedTasks;
-
-    case 'DELETE_TASK':
-      const remainingTasks = tasks.filter((task) => task.id !== action.id);
-      return remainingTasks;
-
-    case 'ADD_REMARKS':
-      const editedRemarks = tasks.map((task) => {
-        if (task.id === action.id) {
-          return {
-            ...task,
-            remarks: action.editedRemarks,
-          };
-        }
-        return task;
-      });
-      return editedRemarks;
-
-    case 'ADD_TASK':
-      const newTask = {
-        id: tasks.length + 1,
-        ...action.task,
-      };
-      const updatedTasks = [newTask, ...tasks];
-      return updatedTasks;
-
-    default:
-      return tasks; // Return the existing tasks array for the default case
-  }
-};
+import TaskReducer from '../reducers/TaskReducer';
 
 // Create context
 const TaskContext = createContext();
